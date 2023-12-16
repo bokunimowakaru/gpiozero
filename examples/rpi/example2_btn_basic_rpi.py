183747b9ca4e265873c5f2289cfbd5891a82f2b2
Wataru KUNINO <git@bokunimo.net>
2023-12-16 10:45:12 +0900

    押下時の処理を分離

--- examples/rpi/example2_btn_basic_rpi.py
+++ examples/rpi/example2_btn_basic_rpi.py
@@ -15,6 +15,9 @@ from RPi import GPIO                            # GPIOモジュールの取得
 from time import sleep                          # スリープ実行モジュールの取得
 from sys import argv                            # 本プログラムの引数argvを取得
 
+def pressed(body='ボタンが押されました'):       # ボタン押下時の処理の定義
+    print(body)                                 # メッセージを表示
+
 print(argv[0])                                  # プログラム名を表示する
 if len(argv) >= 2:                              # 引数があるとき
     port = int(argv[1])                         # 整数としてportへ代入
@@ -30,7 +33,7 @@ try:                                            # キー割り込みの監視を
         while b == 0:                           # ボタンが押されている間
             sleep(0.1)                          # 0.1秒間の待ち時間処理
             b = GPIO.input(port)                # GPIO入力値を変数bへ代入
-        print('ボタンが押されました')           # ボタンが押されましたを表示
+        pressed()                               # ボタン押下時の処理を実行
 except KeyboardInterrupt:                       # キー割り込み発生時
     print('\nKeyboardInterrupt')                # キーボード割り込み表示
 GPIO.cleanup(port)                              # GPIOを未使用状態に戻す
