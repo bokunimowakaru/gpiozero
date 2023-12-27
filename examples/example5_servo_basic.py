986221beac3c5b9899ebf95461a99e0e9257859d
Wataru KUNINO <git@bokunimo.net>
2023-12-27 23:48:27 +0900

    分度器角⇒数学の角度に

--- examples/example5_servo_basic.py
+++ examples/example5_servo_basic.py
@@ -20,7 +20,7 @@
 # ラズベリー・パイに接続した Tower Pro製 マイクロ・サーボモータSG90を
 # キーボードから入力した角度の位置に回転して合わせます。。
 # 回転時に約120mAの電流が流れます(電源5.2V, 無負荷, 実測値, ピーク電流除く)。
-# servo > のプロンプトが表示されたら角度0～180を入力してください。
+# servo > のプロンプトが表示されたら角度-90～90を入力してください。
 # 終了するには[Ctrl]キーを押しながら[C]を押してください。
 
 port = 14                                   # GPIO ポート番号=14 (8番ピン)
@@ -33,10 +33,10 @@ servo = AngularServo(port, min_pulse_width=pwm_min, max_pulse_width=pwm_max)
 
 while True:                                 # 繰り返し処理
     print('servo',end=' > ');               # キーボード入力待ち表示
-    deg_s =input()                          # キーボードから入力
-    if len(deg_s)<0 or not deg_s.isdigit(): # 入力が数字でないとき
+    try:                                    # 例外処理の監視開始
+        deg = int(input())                  # キーボードから入力
+    except ValueError:
         continue                            # whileの先頭に戻る
-    deg = -(int(deg_s) - 90)                # 角度値0～180を指示値±90に変換
     if deg < -90 or deg > 90:               # ±90度の範囲外のとき
         continue                            # whileの先頭に戻る
     print('PWM('+str(port)+')=', deg)       # ポート番号と変数valを表示
